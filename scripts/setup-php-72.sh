707aad666892de8bf77c96edc46c4e0ac4720eb0
Filipe <filipefernandes007@gmail.com>
2018-11-19 20:14:52 +0000

    Fix copy php-xdebug.ini

--- scripts/setup-php-72.sh
+++ scripts/setup-php-72.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # installing php7.2 and php modules
 sudo apt-get install -y php7.2-cli php7.2-common libapache2-mod-php7.2 php7.2-fpm php7.2-curl php7.2-gd php7.2-bz2 php7.2-json php7.2-tidy php7.2-mbstring php-redis php-memcached
 sudo apt-get install -y php7.2-mysql
@@ -11,6 +12,6 @@ sudo apt-get install -y php-pear
 sudo apt-get install -y php7.2-dev
 sudo apt-get install -y php-xdebug
 
-sudo cp ../config/php-xdebug-ini /etc/php/7.2/mods-available/xdebug.ini
+sudo cp /var/www/html/config/php-xdebug.ini /etc/php/7.2/mods-available/xdebug.ini
 
 sudo service apache2 restart
